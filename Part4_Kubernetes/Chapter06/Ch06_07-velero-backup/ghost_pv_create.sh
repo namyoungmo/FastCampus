188215fa9b49a0dec06cb8ad8c8dc74e79f368c4
namyoungmo <agilemomo@gmail.com>
2023-08-22 12:33:50 +0900

    [fix]security problem

--- Part4_Kubernetes/Chapter06/Ch06_07-velero-backup/ghost_pv_create.sh
+++ Part4_Kubernetes/Chapter06/Ch06_07-velero-backup/ghost_pv_create.sh
@@ -1,4 +1,14 @@
 # before ghost installation by helm, set the aws ebs csi driver 
+# https://repost.aws/knowledge-center/eks-persistent-storage 참조
+# To verify that your AWS IAM OpenID Connect (OIDC) provider exists for your cluster, run the following command:
+aws eks describe-cluster --name your_cluster_name --query "cluster.identity.oidc.issuer" --output text
+
+# Verify that your IAM OIDC provider is configured:
+aws iam list-open-id-connect-providers | grep OIDC_PROVIDER_ID
+
+# Create an IAM OIDC provider:
+eksctl utils associate-iam-oidc-provider --cluster my-cluster --approve
+
 # aws ebs csi driver role
 aws iam create-role  --role-name AmazonEKS_EBS_CSI_Driver  --assume-role-policy-document file://"trust-policy.json"
 
